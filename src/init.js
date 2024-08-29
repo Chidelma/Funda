75be32e2a73417a46a3b8e0e9145e7cb6798e621
chidelma <samuel@clickspace.com>
2024-08-29 10:00:06 -0400

    feat: mkdir instead of copy

--- src/init.js
+++ src/init.js
@@ -1,11 +1,11 @@
-import { rename } from 'node:fs/promises'
+import { rename, mkdir } from 'node:fs/promises'
 
 await Promise.allSettled([
-    rename(`${process.cwd()}/node_modules/@vyckr/funda/src/pages`, `${process.cwd()}/src/pages`),
-    rename(`${process.cwd()}/node_modules/@vyckr/funda/src/scripts`, `${process.cwd()}/scripts`),
-    rename(`${process.cwd()}/node_modules/@vyckr/funda/src/components`, `${process.cwd()}/components`),
-    rename(`${process.cwd()}/node_modules/@vyckr/funda/src/layouts`, `${process.cwd()}/layouts`),
-    rename(`${process.cwd()}/node_modules/@vyckr/funda/src/assets`, `${process.cwd()}/assets`),
+    mkdir(`${process.cwd()}/pages`, { recursive: true }),
+    mkdir(`${process.cwd()}/scripts`, { recursive: true }),
+    mkdir(`${process.cwd()}/components`, { recursive: true }),
+    mkdir(`${process.cwd()}/layouts`, { recursive: true }),
+    mkdir(`${process.cwd()}/assets`, { recursive: true }),
     rename(`${process.cwd()}/node_modules/@vyckr/funda/src/app.html`, `${process.cwd()}/app.html`),
     rename(`${process.cwd()}/node_modules/@vyckr/funda/src/app.js`, `${process.cwd()}/app.js`),
 ])
